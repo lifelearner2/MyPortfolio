5dd94a3fdbbd5b1f17794d47e0846fe6c39ae1a3
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-11-05 13:14:27 -0700

    ready for hosting

---
+++ README.md
@@ -0,0 +1,17 @@
+# static_node_and_express_site-v1-2
+(unit 6 of TechDegree)
+
+Jeanene Cannon Portfolio:
+In addition to completing this portfolio as required, I also made the following improvements: 
+1. Changed the background color of the sidebar.
+2. Changed the text color on the sidebar.
+3. Added Background Color to the 'back' button on the header/footer.
+4. Added background color to the Home Page Bio Section.
+5. Added a shadow box to the projects on the Home Page.
+6. Added an H2 Text line to the Home Page.
+7. Changed the text color on the Home Page.
+8. Changed color of the border on the Home Page.
+9. Added a picture to my About Page.
+10. Added a color line to css to change the color of the project titles which were set to standard google purple once clicked. (Advanced code provided to me by Student Support Specialist, Brian Jensen).
+11. Added a special/advanced code in project.pug which allowed me to not just iterate through and post the required two images to my projects, but also the 5 images for one of my projects. This code was provided to me courtesy of Student Support Specialist, Brian Jensen - to replace my code that only populated the required two pics per project.
+
---
+++ app.js
@@ -0,0 +1,60 @@
+// Import Express and set up the app
+const express = require("express");
+const pug = require("pug");
+const app = express();
+const port = process.env.PORT || 3000;
+const {projects} = require("./data/data.json");
+
+//setting up a static (images & stylesheets) & express.static middleware to make the public folder contents available at the root of the application.
+app.use("/static", express.static("public"));
+
+//middleware set up
+app.set("view engine", "pug");
+
+/* GET home page. */
+app.get("/", (req, res) => {
+  res.render("index", { projects: projects });
+});
+
+//About Page route
+app.get("/about", (req, res, next) => {
+  res.render("about");
+  console.log('Handling request to "about" route, "/about"');
+});
+
+/* GET project page. */
+app.get("/project/:id", (req, res, next) => {
+  // res.render("project");
+  console.log('Processing request for "Projects" page');
+  const projectId = req.params.id;
+  const project = projects.find(({ id }) => id === +projectId);
+    if (project) {
+      res.render("project", { project });
+    } else {
+      next();
+    }
+})
+//Handling errors caught by route handlers
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).render('page-not-found', { err });
+    console.log("Handling 404 error");
+  } else {
+    err.message = err.message || `Oops!  It looks like something went wrong on the server.`;
+    res.status(err.status || 500).render('error', { err });
+  }
+});
+
+// Turn on Express Server
+app.listen(port, () => {
+  console.log("Server listening on port 3000");
+});
+
+module.exports = app;
